1d5e0654db245d2f6140b6e650c9fb738df8c374
yahy-a <133936738+yahy-a@users.noreply.github.com>
2024-10-22 17:21:41 +0530

    commit

--- habiter_/lib/providers/habit_provider.dart
+++ habiter_/lib/providers/habit_provider.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
-import 'package:habiter_/database_helper.dart';
+import 'package:habiter_/firebase%20services/firebase_service.dart';
 import 'package:habiter_/models/habit.dart';
 
 class HabitsProvider with ChangeNotifier {
