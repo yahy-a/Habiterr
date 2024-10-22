c9918cbd0249a9244607219ea2a1152fcb41e1f6
yahy-a <133936738+yahy-a@users.noreply.github.com>
2024-10-22 17:36:55 +0530

    commit

--- habiter_/lib/models/habit.dart
+++ habiter_/lib/models/habit.dart
@@ -1,45 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Habit {
-  final int? id; // The ID is required and cannot be null
+  final String? id;
   final String userId;
-  final String name; // Name of the habit
-  final String detail; // Details about the habit
-  final int numberOfDays; // Number of days for the habit
-  final String frequency; // Frequency of the habit
-  bool isCompleted; // Indicates if the habit is completed
+  final String name;
+  final String detail;
+  final String frequency;
+  final int numberOfDays;
+  final DateTime createdAt;
+  final Map<String, HabitEntry> entries;
 
   Habit({
     this.id,
     required this.userId,
     required this.name,
-    this.isCompleted = false,
-    required this.numberOfDays,
-    required this.frequency,
     required this.detail,
-  });
-
-  // Convert a Habit object into a Map object
-  Map<String, dynamic> toMap() {
+    required this.frequency,
+    required this.numberOfDays,
+    DateTime? createdAt,
+    Map<String, HabitEntry>? entries,
+  })  : createdAt = createdAt ?? DateTime.now(),
+        entries = entries ?? {};
+  
+  Map<String,dynamic> toMap(){
     return {
-      'id': id,
-      'userId':userId,
+      'userId': userId,
       'name': name,
       'detail': detail,
       'frequency': frequency,
       'numberOfDays': numberOfDays,
-      'isCompleted': isCompleted ? 1 : 0, // Convert bool to int for SQLite
+      'createdAt': Timestamp.fromDate(createdAt),
     };
   }
 
-  // Convert a Map object into a Habit object
-  factory Habit.fromMap(Map<String, dynamic> map) {
+  factory Habit.fromDocument(DocumentSnapshot doc){
+    final data = doc.data() as Map<String,dynamic>;
     return Habit(
-      id: map['id'],
-      userId: map['userId'],
-      name: map['name'],
-      detail: map['detail'],
-      frequency: map['frequency'],
-      numberOfDays: map['numberOfDays'],
-      isCompleted: map['isCompleted'] == 1, // Convert int back to bool
+      id: doc.id,
+      userId: data['userId'],
+      name: data['name'],
+      detail: data['detail'],
+      frequency: data['frequency'],
+      numberOfDays: data['numberOfDays'],
+      createdAt: (data['createdAt'] as Timestamp).toDate(),
     );
   }
+}
+
+class HabitEntry{
+  final String? id;
+  final String habitId;
+  final DateTime date;
+  final bool isCompleted;
+  final int streak;
+
+  HabitEntry({
+    this.id,
+    required this.habitId,
+    required this.date,
+    this.isCompleted = false,
+    this.streak = 0,
+  });
+
+  Map<String,dynamic> toMap(){
+    return {
+      "habitId" : habitId,
+      "date" : Timestamp.fromDate(date),
+      "isCompleted" : isCompleted,
+      "streak" : streak
+    };
+  }
+
+  factory HabitEntry.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String,dynamic>;
+    return HabitEntry(
+      id: doc.id,
+      habitId: data['habitId'],
+      date: (data['date'] as Timestamp).toDate(),
+      isCompleted: data['isCompleted'] ?? false,
+      streak: data['streak'] ?? 0,
+    );
+  }
+
 }
